a27fcce8b7fde25cb00236da1456415b39d6e400
t62502 <tomasz_matyla@tlen.pl>
2020-03-26 19:18:56 +0000

    Dodano wyświetlanie listy zadań

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var router = require('koa-router')();
 
-router.get('/', function *(next) {
-  yield this.render('index', {
-    title: 'Hello World Koa!'
-  });
+router.get('/', async (ctx, next) => {
+  tasks = await store.listTasks()
+  await ctx.render('index', { tasks })
+});
+
+
 });
 
 router.get('/foo', function *(next) {
@@ -14,3 +16,4 @@ router.get('/foo', function *(next) {
 
 module.exports = router;
 const store = require('../store')
+
--- store.js
+++ store.js
@@ -21,3 +21,21 @@ module.exports = {
   init,
   createTask
 }
+const listTasks = async () => (
+  new Promise((resolve, reject) => {
+    const query = new storage.TableQuery()
+      .select(['title'])
+      .where('PartitionKey eq ?', 'task')
+
+    service.queryEntities(table, query, null, (error, result, response) => {
+      !error ? resolve(result.entries.map((entry) => ({
+        title: entry.title._
+      }))) : reject()
+    })
+  })
+)
+module.exports = {
+  init,
+  createTask,
+  listTasks
+}
